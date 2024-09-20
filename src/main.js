b8f98afd041e267e60048296e84c3656bf2a900f
cristobalwalters12 <cristobalwalters@gmail.com>
2024-09-20 01:33:10 -0300

    Ajustar interceptores Axios en main.js para incluir el token de autenticación en las solicitudes

--- src/main.js
+++ src/main.js
@@ -8,4 +8,18 @@ import store from './store'
 
 loadFonts()
 
+// Interceptores Axios
+axios.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('user-token')
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`
+    }
+    return config
+  },
+  (error) => {
+    return Promise.reject(error)
+  }
+)
+
 createApp(App).use(store).use(router).use(vuetify).mount('#app')
