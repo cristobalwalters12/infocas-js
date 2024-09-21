813105b41d2bd1fd71d9171bfa98b48f1e1fb2f4
cristobalwalters12 <cristobalwalters@gmail.com>
2024-09-21 01:47:27 -0300

    Añadir rutas para agregar, editar y eliminar usuarios en la API

--- src/api/config/apiUrls.js
+++ src/api/config/apiUrls.js
@@ -2,6 +2,10 @@ const ENDPOINT = import.meta.env.VITE_HOST
 
 const API_PATHS = {
   login: '/usuario/login',
+  allUsers: '/usuario',
+  addUser: '/usuario',
+  editUser: '/usuario/:id',
+  deleteUser: '/usuario/:id',
   historial: '/historial',
   rangeInformation: '/sensores/range-information',
   temperatureInformation: '/sensores/temperature-information',
