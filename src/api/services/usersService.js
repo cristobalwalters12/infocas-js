a8060e17127f072cb346aec93303ba06ef3c5ff4
cristobalwalters12 <cristobalwalters@gmail.com>
2024-09-21 01:47:47 -0300

    Añadir funciones getAllUsers, addUser, editUser y deleteUser a usersService.js

--- src/api/services/usersService.js
+++ src/api/services/usersService.js
@@ -10,4 +10,40 @@ const UserLogin = async (user) => {
   }
 }
 
-export { UserLogin }
+const getAllUsers = async () => {
+  try {
+    const response = await api.get(API_PATHS.allUsers)
+    return response.data
+  } catch (error) {
+    return error
+  }
+}
+
+const addUser = async (user) => {
+  try {
+    const response = await api.post(API_PATHS.addUser, user)
+    return response.data
+  } catch (error) {
+    return error
+  }
+}
+const editUser = async (user) => {
+  try {
+    const url = API_PATHS.editUser.replace(':id', user.id)
+    const response = await api.patch(url, user)
+    return response.data
+  } catch (error) {
+    return error
+  }
+}
+const deleteUser = async (user) => {
+  try {
+    const url = API_PATHS.deleteUser.replace(':id', user.id)
+    const response = await api.delete(url, user)
+    return response.data
+  } catch (error) {
+    return error
+  }
+}
+
+export { getAllUsers, UserLogin, addUser, editUser, deleteUser }
