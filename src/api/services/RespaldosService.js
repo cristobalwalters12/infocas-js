dc9a0055391d7e3dda8487c784f6db0bc0bb0607
cristobalwalters12 <cristobalwalters@gmail.com>
2024-11-14 22:00:07 -0300

    Agregar función doRespaldo2024 y mejorar manejo de errores en RespaldosService.js

--- src/api/services/RespaldosService.js
+++ src/api/services/RespaldosService.js
@@ -10,12 +10,19 @@ const getControladores = async () => {
   }
 }
 
-const doResplado = async (respaldo)  =>{
+const doResplado = async (respaldo) => {
   try {
     const response = await api.post(API_PATHS.respaldar, respaldo)
     return response.data
+  } catch (error) {
+    return error
   }
-  catch (error) {
+}
+const doRespaldo2024 = async (respaldo) => {
+  try {
+    const response = await api.post(API_PATHS.respaldarSensores2024, respaldo)
+    return response.data
+  } catch (error) {
     return error
   }
 }
@@ -32,21 +39,26 @@ const getRespaldosControladores = async (respaldo) => {
 const descargarRespaldo = async (respaldo) => {
   try {
     const response = await api.post(API_PATHS.DescargarRespaldos, respaldo, {
-      responseType: 'blob', 
-    });
-    const url = window.URL.createObjectURL(new Blob([response.data]));
-    const link = document.createElement('a');
-    link.href = url;
-    link.setAttribute('download', respaldo.archivo);
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-    return response.data;
+      responseType: 'blob'
+    })
+    const url = window.URL.createObjectURL(new Blob([response.data]))
+    const link = document.createElement('a')
+    link.href = url
+    link.setAttribute('download', respaldo.archivo)
+    document.body.appendChild(link)
+    link.click()
+    document.body.removeChild(link)
+    return response.data
   } catch (error) {
-    console.error('Error al descargar el archivo:', error);
-    throw error;
+    console.error('Error al descargar el archivo:', error)
+    throw error
   }
-};
+}
 
-
-export { getControladores, doResplado, getRespaldosControladores, descargarRespaldo }
+export {
+  getControladores,
+  doResplado,
+  doRespaldo2024,
+  getRespaldosControladores,
+  descargarRespaldo
+}
